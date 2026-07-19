07bdc4a275edcf6f261f05a2ca515da59e0eee3f
Will Molloy <wmolloy15@gmail.com>
2026-07-19 20:53:32 +1000

    Release1.16

--- components/Note.tsx
+++ components/Note.tsx
@@ -6,11 +6,10 @@ export default function Note() {
       <div className="max-w-2xl mx-auto text-center">
         <h2 className="heading text-4xl md:text-5xl mb-4">A Note From Us</h2>
         <p className="body-text text-lg md:text-xl leading-relaxed text-ink/80">
-          Not another wedding... you might be thinking, but we promise this one
-          is going to be special. Welcome to our Save the Date.
+          Welcome to our Save the Date. You are one of our nearest and dearest and we hope you can help us celebrate our special day.
         </p>
         <p className="body-text text-lg md:text-xl leading-relaxed text-ink/80 mt-4">
-          Please keep it free. More details to come!
+          Please keep it free. Formal invite to come!
         </p>
       </div>
     </section>
